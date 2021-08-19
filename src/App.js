c080e719f096d0c7ac981fe38836d519080340b5
Josef <josef.jmay@gmail.com>
2021-08-19 18:12:57 -0400

    Added sign in and sign up page, added form

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import './App.css';
 
 //Components
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.js';
-import Header from  './components/header/header.component.js';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from  './components/header/header.component';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
