101ae7d79a4902a217b9b5cec344a891812f282c
Josef <josef.jmay@gmail.com>
2021-08-31 22:56:28 -0400

    Added framework for users to sign in with e-mail and password and to be stored in Firestore

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from  './components/header/header.component';
-import {auth} from './firebase/firebase.utils';
+import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
@@ -24,26 +24,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
 
-  componentDidMount(){
+  componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+      this.setState({ currentUser: user });
     });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
-
-    this.setState({currentUser: null});
   }
 
-  render(){
+  render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/signin' component={SignInAndSignUpPage}/>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </div>
     );
